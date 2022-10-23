6a0f8cb44413b627e98118953bccb8a947a4950e
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-23 14:05:15 +0530

    demonstration of static variables, methods, and blocks.

---
+++ StaticVariablesMethodsAndBlocks.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.firstday;
+
+class GivenYearIsLeapYearOrNot {
+	  static int a = 3;// demonstration Static variable
+	  static int b;
+	  // demonstration Static method 
+	  static void meth(int x) {
+	    System.out.println("x = " + x);
+	    System.out.println("a = " + a);
+	    System.out.println("b = " + b);
+	  }
+	    // demonstration Static block
+	  static {
+	    System.out.println("Static block initialized.");
+	    b = a * 4;
+	  }
+	   
+	  public static void main(String args[]) {
+	    meth(42);
+	  }
+	}
