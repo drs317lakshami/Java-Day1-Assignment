f3d3f02b40adea00c60e64b59fc42b69d750539c
drs317lakshami <devrajkumar317@gmail.com>
2022-10-23 12:44:12 +0530

    program to display any message

---
+++ DisplayAnyMessage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.firstday;
+
+public class DisplayAnyMessage {
+
+	public static void main(String[] args) {
+		
+	       System.out.println("Hi this is Remaping bach ,expectation is everyone get mapped");
+	}
+}
