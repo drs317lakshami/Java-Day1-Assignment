744e20801b65b908f7f1c362864d0458e9876c40
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-23 13:07:45 +0530

    program to display the default value of all primitive data types of Java

---
+++ DefaultValueOfAllPrimitiveDataType.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.firstday;
+
+
+public class DefaultValueOfAllPrimitiveDataType{
+    byte b; //byte is data types
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bu;
+
+    public static void main(String[] args) {
+
+    	DefaultValueOfAllPrimitiveDataType def = new DefaultValueOfAllPrimitiveDataType();
+        System.out.println(def.b);
+        System.out.println(def.s);
+        System.out.println(def.i);
+        System.out.println(def.l);
+        System.out.println(def.f);
+        System.out.println(def.d);
+        System.out.println(def.c);
+        System.out.println(def.bu);
+    }
+}
