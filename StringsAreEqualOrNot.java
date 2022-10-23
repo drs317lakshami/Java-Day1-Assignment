6e4f43b4117161ed36a461785aa3938979298a34
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-23 13:24:45 +0530

    program to check two strings are equal or not.

---
+++ StringsAreEqualOrNot.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.firstday;
+
+
+
+public class StringsAreEqualOrNot {
+    public static void main(String[] args) {
+
+        String str1 = ("Hello");
+        String str2 = ("Hello");
+        System.out.println(str1.equals(str2));
+    }
+}
